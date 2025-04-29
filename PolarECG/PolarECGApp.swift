5ad59519a478f9eedf0bc8a639899b1997e9cab8
Bruno Gardlo <bruno.gardlo@gmail.com>
2025-04-29 08:45:04 +0200

    Initial Commit

---
+++ PolarECG/PolarECGApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolarECGApp.swift
+//  PolarECG
+//
+//  Created by Bruno Gardlo on 29/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PolarECGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
